6e7f10a03ec397fdc705db015a5456cf2be64318
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-20 12:16:34 -0800

    Create Treehouse class

--- src/Treehouse.js
+++ src/Treehouse.js
@@ -1,5 +1,27 @@
 class Treehouse {
+  constructor(price, beds, bath, location) {
+    this.price = price;
+    this.pricePoint = '';
+    this.bedrooms = beds;
+    this.bathrooms = bath;
+    this.location = location;
+    this.isBooked = false;
+  }
 
+  discountPrice(x) {
+    this.price *= ((100 - x)/100);
+  }
+
+  determinePricePoint() {
+    switch (this.price) {
+      case (this.price <= 100):
+        this.pricePoint = '$';
+      case (100 < this.price <= 200):
+        this,pricePoint = '$$';
+      default:
+        this.pricePoint = '$$$';
+    }
+  }
 }
 
 module.exports = Treehouse;

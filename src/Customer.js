e34578a0050d34025db34b4205ae87a41e35d439
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-20 13:00:00 -0800

    Create customer class

--- src/Customer.js
+++ src/Customer.js
@@ -1,4 +1,20 @@
 class Customer {
+  constructor(budget) {
+    this.budget = budget;
+    this.bookings = [];
+  }
+
+  determineBudget(treehouse) {
+    if (this.budget >= treehouse.price) {
+      this.bookings.push(treehouse);
+      this.budget -= treehouse.price;
+      treehouse.isBooked = true;
+      return true;
+    }
+    console.log('Sorry, you do not have the funds necessary to complete this transaction at this time!');
+    return false;
+  }
+
 
 }
 
